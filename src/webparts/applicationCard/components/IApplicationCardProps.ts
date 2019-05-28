3dd176749cfffaf9e30a59388126ab4ea28d9041
Thibault Ceulemans <thibault.ceulemans@devoteam.lu>
2019-05-28 16:01:31 +0200

    initial commit

---
+++ src/webparts/applicationCard/components/IApplicationCardProps.ts
@@ -0,0 +1,3 @@
+export interface IApplicationCardProps {
+  description: string;
+}
---
+++ src/webparts/applicationCard/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IApplicationCardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ApplicationCardWebPartStrings' {
+  const strings: IApplicationCardWebPartStrings;
+  export = strings;
+}
